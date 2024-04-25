dd6f26b0994e5bbe8fe55a17f7093e480eb01aed
AnushaArunP <166711574+AnushaArunP@users.noreply.github.com>
2024-04-25 17:47:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("bttn").addEventListener("mouseover",function(){
+    alert("click")
+});
+function changeColor(){
+    document.querySelector('.card-body').style.color="blue";
+}
